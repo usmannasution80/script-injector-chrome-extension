1341d5eb2d3b64ededd25e91fc5e52e13566225c
Usman Nasution <usmannasution32@gmail.com>
2024-10-02 20:51:29 +0700

    Optimize codebase

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,53 @@
 (() => {
 
-  let listTab = document.querySelector('[tab="list"]');
-  let editTab = document.querySelector('[tab="edit"]');
   let editIndex = null;
+
+  const toggleTab = tab => {
+    let tabElements = document.querySelectorAll('[tab]');
+    for(let tabElement of tabElements){
+      if(tabElement.getAttribute('tab') !== tab){
+        tabElement.style.display = 'none';
+        continue;
+      }
+      tabElement.style.display = 'block';
+      switch(tab){
+        case 'edit':
+          setEditTab();
+          break;
+        default:
+          editIndex = null;
+          refreshList();
+      }
+    }
+  };
+
+  const setEditTab = () => {
+    if(editIndex){
+      chrome.storage.local.get('scripts', items => {
+
+        if(!items.scripts) return;
+
+        let {
+          regex,
+          regexFlags,
+          script
+        } = items.scripts[editIndex];
+
+        let editTab = document.querySelector('[tab="edit"]');
+
+        editTab.querySelector('[name="regex"]').value = regex;
+        editTab.querySelector('[name="regex_flags"]').value = regex_flags;
+        editTab.querySelector('[name="script"]').value = script;
+
+      });
+    }
+  };
 
   const refreshList = () => {
 
     chrome.storage.local.get('scripts', items => {
       let scripts = items.scripts || [];
+      let listTab = document.querySelector('[tab="list"]');
       let listElement = listTab.querySelector('ul');
       let listElementContent = '';
 
@@ -19,16 +59,8 @@
       let listContentElements = listElement.getElementsByTagName('li');
       for(let i in listContentElements){
         listContentElements[i].onclick = e => {
-          chrome.storage.local.get('scripts', items => {
-            editIndex = i;
-            let {
-              regex,
-              regexFlags,
-              script
-            } = items.scripts[editIndex];
-            listTab.style.display = 'none';
-            editTab.style.display = 'block';
-          });
+          editIndex = i;
+          toggleTab('edit');
         };
       }
 
@@ -36,26 +68,13 @@
 
   };
 
-  refreshList();
+  let tabTogglers = document.querySelectorAll('[toggle="tab"]');
+  for(let tabToggler of tabTogglers)
+    tabToggler.onclick = e => toggleTab(tabToggler.getAttribute('target'));
 
-  listTab.getElementsByTagName('button')[0].onclick = e => {
-    listTab.style.display = 'none';
-    editTab.style.display = 'block';
-    editIndex = null;
-  };
-
-  let [
-    editTabBackButton,
-    saveEditTabButton
-  ] = editTab.getElementsByTagName('button');
-
-  editTabBackButton.onclick = e => {
-    editTab.style.display = 'none';
-    listTab.style.display = 'block';
-  };
-
-  saveEditTabButton.onclick = e => {
+  document.querySelector('[tab="edit"]').querySelector('[name="save"]').onclick = e => {
     chrome.storage.local.get('scripts', items => {
+      let editTab = document.querySelector('[tab="edit"]');
       let scripts = items.scripts || [];
       let script = {
         regex : editTab.querySelector('[name="regex"]').value,
@@ -66,9 +85,11 @@
       else scripts.push(script);
       chrome.storage.local.set({scripts}, () => {
         refreshList();
-        editTabBackButton.click();
+        toggleTab('list');
       });
     });
   };
+
+  refreshList();
 
 })();
